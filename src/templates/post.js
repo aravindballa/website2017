277f45adffae27eb5ec3821b58a3995c915e403c
Aravind Balla <bsaaravind@gmail.com>
2017-10-15 15:06:28 +0530

    Images in post

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Paper from 'material-ui/Paper';
 import Chip from 'material-ui/Chip';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const handleClick = (url) => {
+  window.open(url);
+}
 
 const PostBackground = ({ color }) => (
   <div style={{
@@ -30,6 +35,14 @@ export default function Template({ data }) {
           )}
         </div>
         <div className="post-content" dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div className="actions">
+          {post.frontmatter.source ?
+            (<RaisedButton label="Github" className="btn" onClick={() => handleClick(post.frontmatter.source)} />) :
+            (<span />)}
+          {post.frontmatter.link ?
+            (<RaisedButton label="Open" className="btn" onClick={() => handleClick(post.frontmatter.link)} />) : (<span />)}
+
+        </div>
       </Paper>
     </div>
   )
