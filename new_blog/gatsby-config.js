908faf543fe056d63086685c40461f71714426d4
Aravind Balla <bsaaravind@gmail.com>
2018-12-10 18:03:59 +0530

    change to gatsby starter

--- new_blog/gatsby-config.js
+++ new_blog/gatsby-config.js
@@ -1,21 +1,70 @@
 module.exports = {
   siteMetadata: {
     title: 'Aravind Balla',
+    author: 'Aravind balla',
+    description: 'Discoveries and rants of a developer while developing stuff',
+    siteUrl: 'https://aravindballa.com/',
   },
+  pathPrefix: '/',
   plugins: [
-    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-65268954-3`,
+      },
+    },
+    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'ab-website-2018',
-        short_name: 'Aravind Balla',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/logo.png', // This path is relative to the root of the site.
+        name: `Aravind Balla`,
+        short_name: `AB`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/logo.png`,
       },
     },
-    'gatsby-plugin-offline',
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+    `gatsby-plugin-styled-components`
   ],
 }
