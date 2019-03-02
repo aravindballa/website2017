8734ce8310d6b40dacaa10d6657547ee066b928c
Aravind Balla <bsaaravind@gmail.com>
2019-03-02 14:45:05 +0530

    relocate content directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/content`,
+        path: `${__dirname}/content`,
         name: 'content',
       },
     },
