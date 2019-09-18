f911f27243be0504324e9bdae1776f8af899bd3b
Aravind Balla <bsaaravind@gmail.com>
2019-09-18 10:11:21 +0530

    wip: add visual tests to writings

---
+++ tests/writings.test.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const { readdirSync, statSync } = require('fs');
+const { join } = require('path');
+
+let browser;
+let page;
+
+const dirs = p => readdirSync(p).filter(f => statSync(join(p, f)).isDirectory());
+const slugs = dirs('./content/writings').filter(s => s !== 'demo');
+
+console.log(slugs);
+
+beforeAll(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+});
+
+afterAll(async () => {
+  if (browser) await browser.close();
+});
+
+describe('Writings', () => {
+  for (const slug of slugs) {
+    test(`visually looks correct: ${slug}`, async () => {
+      await page.goto(`http://localhost:8000/writings/${slug}`, { waitUntil: 'networkidle0' });
+      const image = await page.screenshot({ fullPage: true });
+      expect(image).toMatchImageSnapshot();
+    }, 30000);
+  }
+});
